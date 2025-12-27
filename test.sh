6c349d5bafe98d13cb0c64adbf58e8dc4162e7be
YuxuanLiuTier4Desktop <619684051@qq.com>
2025-12-27 13:10:43 +0900

    fix
    Signed-off-by: YuxuanLiuTier4Desktop <619684051@qq.com>

---
+++ test.sh
@@ -0,0 +1,7 @@
+# Set your API key
+export GOOGLE_API_KEY="AIzaSyCJKQ76H09uFaiea6GlKf23wTYfM72ZhtY"
+ 
+# Generate slides
+python scripts/test_local.py \
+    --pdf /home/ukenryu/python_try_new/paper2slide/2405.01533v2.pdf \
+    --output presentations/output/
